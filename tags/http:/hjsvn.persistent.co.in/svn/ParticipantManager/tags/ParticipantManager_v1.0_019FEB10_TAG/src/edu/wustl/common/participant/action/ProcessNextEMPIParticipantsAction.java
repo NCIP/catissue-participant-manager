b481acdf609d32323c92142e3b66702299d1ac06
Geeta Jaggal <geeta_jaggal>
2010-02-19 08:26:41 +0000

    Resolved the eMPI bugs for v.1.1rc
    SVN-Revision: 88

---
+++ tags/http:/hjsvn.persistent.co.in/svn/ParticipantManager/tags/ParticipantManager_v1.0_019FEB10_TAG/src/edu/wustl/common/participant/action/ProcessNextEMPIParticipantsAction.java
@@ -0,0 +1,81 @@
+/**
+ *
+ */
+
+package edu.wustl.common.participant.action;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.common.action.SecureAction;
+import edu.wustl.common.beans.SessionDataBean;
+import edu.wustl.common.participant.utility.Constants;
+import edu.wustl.common.participant.utility.ParticipantManagerUtility;
+
+/**
+ * @author suhas_khot
+ *
+ */
+public class ProcessNextEMPIParticipantsAction extends SecureAction
+{
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	public ActionForward executeSecureAction(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		Long identifier = null;
+		String pageOf = null;
+		ActionForward actionFwd = null;
+		final SessionDataBean sessionDataBean = (SessionDataBean) request.getSession()
+				.getAttribute(edu.wustl.common.util.global.Constants.SESSION_DATA);
+		final Long userId = sessionDataBean.getUserId();
+		List<Long> participantIds = ParticipantManagerUtility
+				.getProcessedMatchedParticipantIds(userId);
+		ActionForward actionforward = new ActionForward();
+		if (participantIds != null && !participantIds.isEmpty())
+		{
+			Long participantId = null;
+			Object nextMatchPartId = request.getSession().getAttribute(
+					Constants.NEXT_PART_ID_TO_PROCESS);
+			if (nextMatchPartId != null && participantIds.contains(nextMatchPartId))
+			{
+				participantId = (Long) nextMatchPartId;
+			}
+			else
+			{
+				participantId = (Long) participantIds.get(0);
+			}
+			identifier = Long.valueOf(participantId.toString());
+			pageOf = "pageOfMatchedParticipant";
+			request.setAttribute(edu.wustl.common.util.global.Constants.SYSTEM_IDENTIFIER,
+					identifier);
+			request.setAttribute(Constants.PAGE_OF, pageOf);
+
+			actionFwd = mapping.findForward(Constants.SUCCESS);
+			actionforward.setContextRelative(false);
+			actionforward.setName(actionFwd.getName());
+			String path = actionFwd.getPath() + "?id=" + identifier + "&pageOf=" + pageOf;
+			actionforward.setPath(path);
+			actionforward.setRedirect(true);
+		}
+		else
+		{
+			pageOf="pageOfMatchedParticipant";
+			actionFwd = mapping.findForward("ProcessMatchedParticipants");
+			actionforward.setContextRelative(false);
+			actionforward.setName(actionFwd.getName());
+			String path = actionFwd.getPath()+ "?pageOf=" + pageOf + "&identifierFieldIndex="+0;
+			actionforward.setPath(path);
+			actionforward.setRedirect(true);
+		}
+		return actionforward;
+	}
+}
---
+++ tags/http:/hjsvn.persistent.co.in/svn/ParticipantManager/tags/ParticipantManager_v1.0_019FEB10_TAG/src/edu/wustl/common/participant/utility/ParticipantMatchingTimerTask.java
@@ -0,0 +1,78 @@
+
+package edu.wustl.common.participant.utility;
+
+import java.util.List;
+import java.util.TimerTask;
+
+import edu.wustl.common.participant.bizlogic.ParticipantMatchingBizLogic;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.dao.JDBCDAO;
+import edu.wustl.dao.exception.DAOException;
+
+/**
+ * @author geeta_jaggal.
+ * The Class ParticipantMatchingTimerTask :Used to perform the CIDER participants match
+ * asynchronously.
+ */
+public class ParticipantMatchingTimerTask extends TimerTask
+{
+
+	private static final Logger logger = Logger.getCommonLogger(ParticipantMatchingTimerTask.class);
+
+	/* (non-Javadoc)
+	 * @see java.util.TimerTask#run()
+	 */
+	public void run()
+	{
+		try
+		{
+			perFormParticipantMatch();
+		}
+		catch (Exception e)
+		{
+
+			//logger.info(e.getMessage());
+			System.out.println("Error during participant timer task");
+		}
+	}
+
+	/**
+	 * Per form participant match.
+	 *
+	 * @throws Exception the exception
+	 */
+	private void perFormParticipantMatch() throws Exception
+	{
+		List participantIdList = fetchSearchParticipantIds();
+		if (!participantIdList.isEmpty() && participantIdList.get(0) != "")
+		{
+			ParticipantMatchingBizLogic bizLogic = new ParticipantMatchingBizLogic();
+			bizLogic.perFormParticipantMatch(participantIdList);
+		}
+	}
+
+	/**
+	 * Fetch search participant ids.
+	 *
+	 * @return the list
+	 *
+	 * @throws DAOException the DAO exception
+	 */
+	private List fetchSearchParticipantIds() throws DAOException
+	{
+
+		JDBCDAO dao = null;
+		List idList = null;
+		try
+		{
+			dao = ParticipantManagerUtility.getJDBCDAO();
+			String query = "SELECT SEARCHED_PARTICIPANT_ID FROM MATCHED_PARTICIPANT_MAPPING WHERE NO_OF_MATCHED_PARTICIPANTS='-1'";
+			idList = dao.executeQuery(query);
+		}
+		finally
+		{
+			dao.closeSession();
+		}
+		return idList;
+	}
+}
