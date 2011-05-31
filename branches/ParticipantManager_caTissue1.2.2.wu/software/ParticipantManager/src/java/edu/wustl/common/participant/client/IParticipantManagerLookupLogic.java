9f2017625a4e98b2aa9bf0e96d67cda84863da3d
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-31 05:39:50 +0000

    for catissue release 1.2.2.wu
    SVN-Revision: 200

---
+++ branches/ParticipantManager_caTissue1.2.2.wu/software/ParticipantManager/src/java/edu/wustl/common/participant/client/IParticipantManagerLookupLogic.java
@@ -0,0 +1,18 @@
+package edu.wustl.common.participant.client;
+
+import java.util.List;
+import java.util.Set;
+
+import edu.wustl.common.lookup.LookupLogic;
+import edu.wustl.common.lookup.LookupParameters;
+
+
+public interface IParticipantManagerLookupLogic extends LookupLogic
+{
+	/**
+	 * @param params Lookup Parameters.
+	 * @return List
+	 * @throws Exception Exception.
+	 */
+	List lookup(LookupParameters params,Set<Long> csList) throws Exception;
+}
