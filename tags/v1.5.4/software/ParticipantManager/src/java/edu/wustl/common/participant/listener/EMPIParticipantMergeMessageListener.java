5c71d4ddd075de41ed597f31a2e551dc0a07d93d
Pavan Kalantri <kalantrp>
2012-06-29 16:53:17 +0000

    tag for clinportal v2.5
    SVN-Revision: 300

---
+++ tags/v1.5.4/software/ParticipantManager/src/java/edu/wustl/common/participant/listener/EMPIParticipantMergeMessageListener.java
@@ -0,0 +1,385 @@
+
+
+package edu.wustl.common.participant.listener;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.exception.BizLogicException;
+import edu.wustl.common.participant.bizlogic.CommonParticipantBizlogic;
+import edu.wustl.common.participant.domain.IParticipant;
+import edu.wustl.common.participant.domain.IParticipantMedicalIdentifier;
+import edu.wustl.common.participant.domain.ISite;
+import edu.wustl.common.participant.utility.Constants;
+import edu.wustl.common.participant.utility.ParticipantManagerException;
+import edu.wustl.common.participant.utility.ParticipantManagerUtility;
+import edu.wustl.common.participant.utility.PropertyHandler;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.dao.JDBCDAO;
+import edu.wustl.dao.exception.DAOException;
+import edu.wustl.dao.query.generator.ColumnValueBean;
+import edu.wustl.dao.query.generator.DBTypes;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The listener interface for receiving EMPIParticipantMergeMessage events.
+ * The class that is interested in processing a EMPIParticipantMergeMessage
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addEMPIParticipantMergeMessageListener<code> method. When
+ * the EMPIParticipantMergeMessage event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see EMPIParticipantMergeMessageEvent
+ */
+public class EMPIParticipantMergeMessageListener implements MessageListener
+{
+
+	/** The Constant logger. */
+	private static final Logger LOGGER = Logger
+			.getCommonLogger(EMPIParticipantMergeMessageListener.class);
+
+	/**
+	 * This method will be called as soon as the participant demographic message arrives in the queue.
+	 * Read the message from queue and update the participant.
+	 *
+	 * @param message the message
+	 */
+	public void onMessage(final Message message)
+	{
+
+		String mergeMessage = "";
+		Map<String, String> messageValueMap = null;
+		try
+		{
+			if (message instanceof TextMessage)
+			{
+				mergeMessage = ((TextMessage) message).getText();
+				LOGGER.info("Received merge message---------------------- \n \n");
+				LOGGER.info(mergeMessage);
+				messageValueMap = new HashMap<String, String>();
+				getMessageToknized(mergeMessage, messageValueMap);
+				processMergeMessage(messageValueMap);
+				LOGGER.info("Processed merge message--------------------");
+			}
+		}
+		catch (JMSException exp)
+		{
+			LOGGER.error(Constants.PROCESS_ERROR + mergeMessage);
+			LOGGER.error(exp.getMessage(),exp);
+		}
+		catch (Exception be)
+		{
+			LOGGER.error(Constants.PARTICIPANT_ERROR + messageValueMap.get(Constants.OLD_EMPIID));
+			LOGGER.error(be.getMessage(),be);
+		}
+
+	}
+
+	/**
+	 * Create a map for the corrosponding value in message.
+	 *
+	 * @param mergeMessage the merge message received from CDR
+	 * @param valueMap of the different parameters received in message
+	 */
+	private void getMessageToknized(final String mergeMessage, final Map<String, String> valueMap)
+	{
+		final StringTokenizer strTokenizer = new StringTokenizer(mergeMessage, "\r");
+		valueMap.put(Constants.MERGE_MESSAGE, mergeMessage);
+		while (strTokenizer.hasMoreTokens())
+		{
+			final String token = strTokenizer.nextToken().trim();
+			if(!"".equals(token))
+			{
+			final StringTokenizer strTokenizer1 = new StringTokenizer(token, "|");
+			final String token1 = strTokenizer1.nextToken().trim();
+			if (token1.equalsIgnoreCase("EVN"))
+			{
+				valueMap.put(Constants.hl7EventType, strTokenizer1.nextToken());
+			}
+			else if (token1.equalsIgnoreCase("PID"))
+			{
+				strTokenizer1.nextToken();
+				final String newId = getEMPIId(strTokenizer1.nextToken());
+				valueMap.put(Constants.NEW_EMPIID, newId);
+				if (valueMap.get(Constants.hl7EventType).equalsIgnoreCase("A34"))
+				{
+					final String[] siteMRN = getSiteMRN(strTokenizer1.nextToken());
+					valueMap.put(Constants.NEW_SITE, siteMRN[1]);
+					valueMap.put(Constants.NEW_MRN, siteMRN[0]);
+				}
+			}
+			else if (token1.equalsIgnoreCase("MRG"))
+			{
+				strTokenizer1.nextToken();
+				final String oldId = getEMPIId(strTokenizer1.nextToken());
+				valueMap.put(Constants.OLD_EMPIID, oldId);
+			}
+			}
+		}
+	}
+
+	/**
+	 * Gets the site and MRN from siteMRN string.
+	 *
+	 * @param newSiteMRN the newSiteMRN String which contains site and MRN.
+	 *
+	 * @return  string array of site and MRN
+	 */
+
+	private String[] getSiteMRN(final String newSiteMRN)
+	{
+		String[] siteMRN = new String[2];
+		StringTokenizer strTokenizer = new StringTokenizer(newSiteMRN, "^^^");
+		siteMRN[0] = strTokenizer.nextToken();
+		final String siteString = strTokenizer.nextToken();
+		strTokenizer = new StringTokenizer(siteString, "&");
+		strTokenizer.nextToken();
+		siteMRN[1] = strTokenizer.nextToken();
+		return siteMRN;
+	}
+
+
+	/**
+	 * Gets the eMPI id.
+	 *
+	 * @param identifier the identifier
+	 *
+	 * @return the eMPI id
+	 */
+	private String getEMPIId(final String identifier)
+	{
+		String empiId = "";
+		final StringTokenizer strTokenizer = new StringTokenizer(identifier, "^^^");
+		empiId = strTokenizer.nextToken();
+		empiId = removePreAppZeroes(empiId);
+		return empiId;
+	}
+
+	/**
+	 * Removes the pre app zeroes.
+	 *
+	 * @param identifier the identifier
+	 *
+	 * @return the string
+	 */
+	private String removePreAppZeroes(final String identifier)
+	{
+		String empiId;
+		for (empiId = identifier; empiId.length() > 0 && empiId.charAt(0) == '0'; empiId = empiId
+				.substring(1))
+		{
+		}
+		return empiId;
+	}
+
+	/**
+	 * Store merge message.
+	 *
+	 * @param jdbcdao the jdbcdao
+	 * @param hl7Message the hl7 message
+	 * @param messageType the message type
+	 * @param status the status
+	 *
+	 * @throws DAOException the DAO exception
+	 */
+	private void storeMergeMessage( final String hl7Message,
+			final String messageType ,final String status)
+			throws DAOException
+	{
+		long idenifier = 0L;
+		String insQuery = "";
+		String identifier = "";
+		String query =null;
+		JDBCDAO jdbcdao = ParticipantManagerUtility.getJDBCDAO();
+		try
+		{
+			query = "SELECT MAX(IDENTIFIER) from PARTICIPANT_MERGE_MESSAGES";
+			final List maxIdList = jdbcdao.executeQuery(query,null,null);
+			if (!maxIdList.isEmpty())
+			{
+				final List idList = (List) maxIdList.get(0);
+				if (!idList.isEmpty() && idList.get(0) != null && !"".equals(idList.get(0)))
+				{
+					identifier = (String) idList.get(0);
+					idenifier = Long.valueOf(identifier).longValue() + 1L;
+				}
+			}
+			final Calendar cal = Calendar.getInstance();
+			final java.util.Date date = cal.getTime();
+			if (idenifier == 0L)
+			{
+				idenifier = 1L;
+			}
+			insQuery = "INSERT INTO PARTICIPANT_MERGE_MESSAGES VALUES(?,?,?,?,?)";
+			final LinkedList<LinkedList<ColumnValueBean>> columnValueBeans = new LinkedList<LinkedList<ColumnValueBean>>();
+			final LinkedList<ColumnValueBean> colValBeanList = new LinkedList<ColumnValueBean>();
+			colValBeanList.add(new ColumnValueBean("IDENTIFIER", Long.valueOf(idenifier),DBTypes.INTEGER));
+			colValBeanList.add(new ColumnValueBean("MESSAGE_TYPE", messageType, DBTypes.VARCHAR));
+			colValBeanList.add(new ColumnValueBean("MESSAGE_DATE", date, DBTypes.DATE));
+			colValBeanList.add(new ColumnValueBean("HL7_MESSAGE", hl7Message,DBTypes.VARCHAR));
+			colValBeanList.add(new ColumnValueBean("MESSAGE_STATUS", status, DBTypes.VARCHAR));
+			columnValueBeans.add(colValBeanList);
+			jdbcdao.executeUpdate(insQuery, columnValueBeans);
+			jdbcdao.commit();
+			jdbcdao.closeSession();
+			LOGGER.info("\n \n  ----------- STORED MERGE MESSAGE ----------  \n\n");
+			LOGGER.info(hl7Message);
+		}
+		catch (DAOException e)
+		{
+			LOGGER
+					.info("\n \n --------  ERROR WHILE STORING THE FOLLOWING MERGE MESSAGE ----------\n\n\n");
+			LOGGER.info(hl7Message);
+			LOGGER.info(e.getMessage());
+			throw new DAOException(e.getErrorKey(), e, e.getMessage());
+		}
+	}
+
+	/**
+	 * Process merge message.
+	 *
+	 * @param mergeMessageMap the merge message map
+	 *
+	 * @return true, if successful
+	 * @throws DAOException
+	 * @throws BizLogicException
+	 * @throws DAOException
+	 * @throws BizLogicException
+	 * @throws BizLogicException
+	 * @throws DAOException
+	 */
+	public void processMergeMessage(final Map<String, String> mergeMessageMap)
+			throws BizLogicException, DAOException
+
+	{
+		final CommonParticipantBizlogic bizLogic = new CommonParticipantBizlogic();
+		IParticipant participant = null;
+		participant = bizLogic.getParticipant(mergeMessageMap.get(Constants.OLD_EMPIID));
+		String status = Constants.UNRESOLVED;
+		if ((participant == null))
+		{
+			LOGGER.info(Constants.USER_NOT_EXIST_CLINPORTAL);
+		}
+		else
+		{
+			/*Condition to handle A30 message merge
+			  In case we get message from CDR that eMPI id changed for existing participant.
+			  in ClinPortal database
+			*/
+			final IParticipant oldParticipant = participant;
+			try
+			{
+				if (mergeMessageMap.get(Constants.hl7EventType).equalsIgnoreCase(
+						Constants.HL7_MERGE_EVENT_TYPE_A30))
+				{
+					participant.setEmpiId(mergeMessageMap.get(Constants.NEW_EMPIID));
+				}
+				else if (mergeMessageMap.get(Constants.hl7EventType).equalsIgnoreCase(
+						Constants.HL7_MERGE_EVENT_TYPE_A34))
+				{
+					updateMRN(participant, mergeMessageMap);
+				}
+				/*
+				 *Updating participant in database using common  participantbizlogic.
+				 */
+					new CommonParticipantBizlogic().updateParticipant(participant, oldParticipant);
+					status = Constants.RESOLVED;
+			}
+			//Status for failure reason while updating participant.
+			catch (BizLogicException be)
+			{
+				status = Constants.ERROR_PARTICIPANT_UPDATE;
+			}
+			//Status for failure reason while updating MRN.
+			catch (ApplicationException e)
+			{
+				status = e.getMsgValues();
+			}
+			//Status for failure reason while updating participant or fetching site in updateMRN.
+			catch (ParticipantManagerException e)
+			{
+				status = Constants.PARTICIPANT_ERROR;
+			}
+			storeMergeMessage(mergeMessageMap.get(Constants.MERGE_MESSAGE), mergeMessageMap
+					.get(Constants.hl7EventType), status);
+		}
+
+	}
+
+
+
+	private void updateMRN(final IParticipant participant,
+			final Map<String, String> mergeMessageMap) throws ApplicationException, ParticipantManagerException
+	{
+
+		if (mergeMessageMap.get(Constants.OLD_EMPIID).equals(
+				mergeMessageMap.get(Constants.NEW_EMPIID)))
+		{
+			Collection<IParticipantMedicalIdentifier<IParticipant, ISite>> pmiCollection = participant
+					.getParticipantMedicalIdentifierCollection();
+			Iterator<IParticipantMedicalIdentifier<IParticipant, ISite>> itr = pmiCollection
+					.iterator();
+			/*
+			 * checking the MRN with the given Site and to set new MRN.
+			 */
+			IParticipantMedicalIdentifier<IParticipant, ISite> pmiToBeUpdated = null;
+			while (itr.hasNext())
+			{
+				IParticipantMedicalIdentifier<IParticipant, ISite> pmi = itr.next();
+				String site = (pmi.getSite()).getName();
+				String mrn = pmi.getMedicalRecordNumber();
+				if (site.equals(mergeMessageMap.get(Constants.NEW_SITE)))
+				{
+					pmiToBeUpdated = pmi;
+					if (!mrn.equals(mergeMessageMap.get(Constants.NEW_MRN)))
+					{
+						pmi.setMedicalRecordNumber(mergeMessageMap.get(Constants.NEW_MRN));
+						break;
+					}
+					else
+					{
+						throw new ApplicationException(null, null,Constants.MRN_EXIST);
+					}
+				}
+			}
+
+			if (pmiToBeUpdated == null)
+			{
+				try
+				{
+					ISite site = new CommonParticipantBizlogic().getSite(mergeMessageMap
+							.get(Constants.NEW_SITE));
+					String pmiClassName = PropertyHandler.getValue(Constants.PMI_CLASS);
+					pmiToBeUpdated = (IParticipantMedicalIdentifier<IParticipant, ISite>) ParticipantManagerUtility
+							.getObject(pmiClassName);
+					pmiToBeUpdated.setMedicalRecordNumber(mergeMessageMap.get(Constants.NEW_MRN));
+					pmiToBeUpdated.setSite(site);
+					pmiToBeUpdated.setParticipant(participant);
+					pmiCollection.add(pmiToBeUpdated);
+				}
+				catch (BizLogicException be)
+				{
+					throw new ApplicationException(null, be,Constants.SITE_ERROR);
+				}
+			}
+		}
+		else
+		{
+			LOGGER.info(Constants.USECASE_NOTSUPPORTED);
+			throw new ApplicationException(null, null,Constants.USECASE_NOTSUPPORTED);
+		}
+	}
+}
---
+++ tags/v1.5.4/software/ParticipantManager/src/java/edu/wustl/patientLookUp/lookUpServiceBizLogic/PatientInfoByName.java
@@ -0,0 +1,96 @@
+
+package edu.wustl.patientLookUp.lookUpServiceBizLogic;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.language.Metaphone;
+
+import edu.wustl.common.participant.utility.ParticipantManagerUtility;
+import edu.wustl.patientLookUp.domain.PatientInformation;
+import edu.wustl.patientLookUp.queryExecutor.IQueryExecutor;
+import edu.wustl.patientLookUp.util.PatientLookupException;
+import edu.wustl.patientLookUp.util.Utility;
+
+/**
+ * This class will searches the patients based on the LastName  value
+ * and returns all the lastName matched patients.
+ * @author geeta_jaggal
+ */
+public class PatientInfoByName
+{
+
+	private Map<String, PatientInformation> patientDataMap = new LinkedHashMap<String, PatientInformation>();
+	edu.wustl.common.util.logger.Logger log = edu.wustl.common.util.logger.Logger.getCommonLogger(PatientInfoByName.class);
+
+	/**
+	 * This method will perform the patient match on lastName and also perform the
+	 * phonetic match on lastName and returns the lastName matched patients.
+	 * @param patientInformation : object which contains user entered patient information.
+	 * @param queryExecutor : object of query executor class.
+	 * @param threshold :cutoff points value.
+	 * @param maxNoOfRecords :max no of records to be returned by algorithm.
+	 * @return List of all lastName matched patients.
+	 * @throws PatientLookupException
+	 */
+	public List<PatientInformation> performMatchOnName(PatientInformation patientInformation,
+			IQueryExecutor queryExecutor, int threshold, int maxNoOfRecords)
+			throws PatientLookupException
+	{
+
+		List<PatientInformation> matchedPatientsByName = new ArrayList<PatientInformation>();
+		List<PatientInformation> matchedPatientsByMetaPhone = null;
+		List<PatientInformation> matchedParticipantList = new ArrayList<PatientInformation>();
+		try
+		{
+
+			//added a empty check for fixing bug :22880
+			if(patientInformation.getLastName() !=null && !"".equals(patientInformation.getLastName()))
+			{
+				String lastName = Utility.compressLastName(patientInformation.getLastName());
+				matchedPatientsByName.addAll(queryExecutor.executeQueryForName(lastName, patientInformation
+						.getProtocolIdSet(), patientInformation.getParticipantObjName()));
+			}
+
+			// get meta phone value
+			Metaphone metaPhoneObj = new Metaphone();
+			String metaPhone = metaPhoneObj.metaphone(patientInformation.getLastName());
+			// Search for metaPhoen matched records
+			if (metaPhone != null && metaPhone != "")
+			{
+				matchedPatientsByMetaPhone = queryExecutor.executetQueryForPhonetic(metaPhone,
+						patientInformation.getProtocolIdSet(), patientInformation
+								.getParticipantObjName());
+				matchedPatientsByName.addAll(matchedPatientsByMetaPhone);
+			}
+			log.debug("***** After executetQueryForPhonetic ****" );
+			Long time1=System.currentTimeMillis();
+			List<Long> facilityIdList= ParticipantManagerUtility.getFacilityIds(patientInformation);
+			log.debug("@@@@@@@@@ Score calculation started from mactches based on NAME @@@@@@@@@@@@@@@@@@@@@");
+			log.debug("no of participants on Mathches On NAME ="+matchedPatientsByName.size());
+
+			Utility.calculateScore(matchedPatientsByName, patientInformation);
+			Long time2=System.currentTimeMillis();
+			log.debug("Time taken for calculateScore::"+(time2-time1));
+			Utility.sortListByScore(matchedPatientsByName);
+			Long time3=System.currentTimeMillis();
+			log.debug("Time taken for sortListByScore::"+(time3-time2));
+			matchedPatientsByName = Utility.processMatchingListForFilteration(
+					matchedPatientsByName, threshold, maxNoOfRecords);
+			log.debug("no of patients on NAme matches after filteration based on score "+matchedPatientsByName.size());
+			log.debug("@@@@@@@@@ Score calculation ends from mactches based on NAME @@@@@@@@@@@@@@@@@@@@@");
+			Utility.populatePatientDataMap(matchedPatientsByName, patientDataMap);
+			Long time4=System.currentTimeMillis();
+			log.debug("Time taken for processMatchingListForFilteration::"+(time4-time3));
+			matchedParticipantList.addAll(patientDataMap.values());
+ 			queryExecutor.fetchRegDateFacilityAndMRNOfPatient(matchedPatientsByName,facilityIdList);
+			return matchedParticipantList;
+		}
+		catch (Exception e)
+		{
+			throw new PatientLookupException(e.getMessage(), e);
+		}
+	}
+}
