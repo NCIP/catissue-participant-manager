069f3c7b6eae89f0ddcea691e9fa0842afa99577
Prasad Konka <prasad.konka@nih.gov>
2013-06-07 09:59:26 -0400

    Added copyright header

--- software/ParticipantManager/test/unit/src/edu/wustl/common/participant/bizlogic/test/CommonParticipantBizLogicTest.java
+++ software/ParticipantManager/test/unit/src/edu/wustl/common/participant/bizlogic/test/CommonParticipantBizLogicTest.java
@@ -1,3 +1,13 @@
+/*L
+ *  Copyright Washington University in St. Louis
+ *  Copyright SemanticBits
+ *  Copyright Persistent Systems
+ *  Copyright Krishagni
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/catissue-participant-manager/LICENSE.txt for details.
+ */
+
 package edu.wustl.common.participant.bizlogic.test;
 
 import static org.powermock.api.easymock.PowerMock.expectLastCall;
